4cac9078178075091064118b8353f0bcb34b9018
Fredirak <frederik.niederhoff@gmx.de>
2022-04-07 14:03:36 +0200

    Anmeldung fertig

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function sendEmail(){
 		Host : "smtp.gmail.com",
 		Username : "milf69.hunter187@gmail.com",
 		Password : "greenjoker",
-		To : 'milf69.hunter187@gmail.com',
+		To : 'Frederik.Niederhoff@gmx.de',
 		From : document.getElementById("email").value,
 		Subject : "New Contact Form",
 		Body : "Name: " + document.getElementById("name").value + 
-				"<br> Email: " + document.getElementById("email").value + 
+				"<br> Email: " + document.getElementById("email").value +
+				"<br> Name Gruppe:  " + document.getElementById("gruppe").value +
+				"<br> Anzahl Mitspieler: " + document.getElementById("number").value +
 				"<br> Nachricht: " + document.getElementById("message").value
 	}).then(
 	  message => alert('Anmeldung wurde verschickt')

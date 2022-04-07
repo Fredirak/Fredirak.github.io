0fd424270188efa3c67d172d6f17ad25ee618b9c
Fredirak <65224702+Fredirak@users.noreply.github.com>
2022-04-07 22:32:34 +0200

    loll

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function sendEmail(){
 		Host : "smtp.gmail.com",
 		Username : "milf69.hunter187@gmail.com",
 		Password : "greenjoker",
-		To : 'milf69.hunter187@gmail.com',
+		To : 'greenjoker.upmv@gmail.com',
 		From : document.getElementById("email").value,
 		Subject : "New Contact Form",
 		Body : "Name: " + document.getElementById("name").value + 

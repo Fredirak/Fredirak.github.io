12c2a77a7f3e94b5689c84385e872e85697a8446
Fredirak <65224702+Fredirak@users.noreply.github.com>
2022-04-07 22:32:04 +0200

    lol

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function sendEmail(){
+function sendEmail(){	
 	Email.send({
 		Host : "smtp.gmail.com",
 		Username : "milf69.hunter187@gmail.com",
 		Password : "greenjoker",
-		To : 'greenjoker.upmv@gmail.com',
+		To : 'milf69.hunter187@gmail.com',
 		From : document.getElementById("email").value,
 		Subject : "New Contact Form",
 		Body : "Name: " + document.getElementById("name").value + 

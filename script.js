aae3f352712799ec5b71627e3c46bc47fbad1fc2
Fredirak <65224702+Fredirak@users.noreply.github.com>
2022-04-07 22:12:40 +0200

    email fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function sendEmail(){
 		Host : "smtp.gmail.com",
 		Username : "milf69.hunter187@gmail.com",
 		Password : "greenjoker",
-		To : 'Frederik.Niederhoff@gmx.de',
+		To : 'greenjoker.upmv@gmail.com',
 		From : document.getElementById("email").value,
 		Subject : "New Contact Form",
 		Body : "Name: " + document.getElementById("name").value + 

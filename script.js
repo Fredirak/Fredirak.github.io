53faf20ffbb101bc0f9b58ac086537d1ca175cfa
Fredirak <65224702+Fredirak@users.noreply.github.com>
2022-05-01 20:05:31 +0200

    Anmeldung fix

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  function sendEmail(){	
 	Email.send({
 		Host : "smtp.gmail.com",
-		Username : "milf69.hunter187@gmail.com",
-		Password : "greenjoker",
+		Username : "warum@gmail.com",
+		Password : "Schreib uns mal bei greenjoker.upmv@gmail.com mich interesseierts wer sowas macht!",
 		To : 'greenjoker.upmv@gmail.com',
 		From : document.getElementById("email").value,
 		Subject : "New Contact Form",
@@ -12,6 +12,6 @@
 				"<br> Anzahl Mitspieler: " + document.getElementById("number").value +
 				"<br> Nachricht: " + document.getElementById("message").value
 	}).then(
-	  message => alert('Anmeldung wurde verschickt. Sie erhalten innerhalb der nächsten 24-48 Stunden eine Antwort von unserem Team.')
+	  message => alert('Anmeldung wurde verschickt. Sie erhalten innerhalb der nächsten 24-48 Stunden eine Antwort von unserem Team. Wenn Sie keine Email von uns erhalten kontaktieren sie uns bitte über unsere im Impressum angegebene Email-Adresse!')
 	);
 }
